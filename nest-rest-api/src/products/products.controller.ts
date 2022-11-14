223899e1e0a96b683a393a6edc7dc5c6b95f2f8f
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-11-14 17:42:11 +0300

    w14.11.2022

--- nest-rest-api/src/products/products.controller.ts
+++ nest-rest-api/src/products/products.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put, Redirect, Req, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put/*, Redirect, Req, Res*/ } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 // import {Request, Response} from 'express'
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
@@ -16,12 +17,12 @@ export class ProductsController {
   //   return 'getAll'
   // }
   @Get()
-  getAll(): ProductsService[] {
+  getAll(): Promise<Product[]> {
     return this.productService.getAll()
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
+  getOne(@Param('id') id: string): Promise<Product> {
     return this.productService.getById(id)
   }
 
@@ -33,17 +34,17 @@ export class ProductsController {
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productService.create(createProductDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return 'Remove ' + id 
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productService.remove(id) 
   }
 
   @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-    return `update id: ${id} (title: ${updateProductDto.title} price: ${updateProductDto.price})`
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): Promise<Product> {
+    return this.productService.update(id, updateProductDto)
   }
 }
